19a7409e764fe9f48b662bc688000db7f424ead5
msyahrulsp <mssp892@gmail.com>
2022-07-15 00:54:35 +0700

    Add routing and Animated transition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { routes } from './util/route';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Navbar } from './component/Navbar';
+
+const pageAnimation = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+const AnimatedRoute = () => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        variants={pageAnimation}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={
+          {
+            duration: 0.3,
+          }
+        }
+      >
+        <Routes>
+          {routes.map((route) => {
+            return (
+                <Route
+                  exact
+                  key={route.path}
+                  path={route.path}
+                  element={<route.component />}
+                />
+            );
+          })}
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  )
+}
 
 function App() {
   return (
-    <Box>
-      Pie Kabare
-    </Box>
+    <Router>
+      <Flex
+        // TODO cek size
+        direction={{ base: 'column', md: 'row' }}
+        h="100%"
+      >
+        <Navbar />
+        <AnimatedRoute />
+      </Flex>
+    </Router>
   );
 }
 
---
+++ src/util/route.js
@@ -0,0 +1,13 @@
+import { Home } from '../pages/Home';
+import { Dashboard } from '../pages/Dashboard';
+
+export const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+  }
+]
