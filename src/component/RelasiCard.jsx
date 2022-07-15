f47968e27eb9c6fdbbef70cec1bb8f36ac7f3fe1
msyahrulsp <mssp892@gmail.com>
2022-07-16 00:11:28 +0700

    Relasi card done

---
+++ src/component/RelasiCard.jsx
@@ -0,0 +1,60 @@
+import { Flex, Text } from '@chakra-ui/react';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+
+const data = [
+  {
+    number: 20,
+    legend: "Memiliki",
+  },
+  {
+    number: 108,
+    legend: "Menggunakan",
+  },
+  {
+    number: 67,
+    legend: "Meminjam",
+  },
+]
+
+export const RelasiCard = () => {
+  return (
+    <Flex
+      px={6}
+      py={5}
+      flexDirection="column"
+      bg="#FFFFFF"
+      boxShadow= "4px 5px 5px -2px lightgray"
+      borderRadius="lg"
+      gap={3}
+      flexWrap="wrap"
+    >
+      <Flex
+        flexDirection="row"
+        justifyContent="space-between"
+        mb={4}
+      >
+        <Text fontWeight="bold" fontSize="lg">
+          Relasi
+        </Text>
+        <Text color="green">
+          Lihat semua
+        </Text>
+      </Flex>
+      {data.map((item) => {
+        return (
+          <Flex flexDirection="row" gap={4} alignItems="center">
+            <AiOutlineShareAlt size={30} />
+            <Flex flexDirection="column">
+              <Text fontSize="xl" fontWeight="bold">
+                {item.number}
+              </Text>
+              <Text opacity="0.5">
+                {item.legend}
+              </Text>
+            </Flex>
+          </Flex> 
+        )
+      })}
+    </Flex>
+  )
+}
