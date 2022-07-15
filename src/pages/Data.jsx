cddfdd4f7ade3eef655f371aa4069136d0e21260
msyahrulsp <mssp892@gmail.com>
2022-07-16 06:16:08 +0700

    GWS api

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -1,16 +1,103 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Text }from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react';
+import {
+  Text,
+  Container,
+  Flex,
+  Center,
+}from '@chakra-ui/react'
 import { PageLayout } from '../layout/PageLayout';
+import { getDataAPI, getToken, newToken } from '../util/api';
+import { SearchBar } from '../component/Search/SearchBar';
+import { DataCard } from '../component/Card/DataCard';
+import Pagination from '../component/Pagination/Pagination';
+import usePagination from '../component/Pagination/PaginationUtil';
 
 export const Data = () => {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState();
+  const [data, setData] = useState([]);
+  const pagination = usePagination({
+    totalItem: data.length,
+    page,
+    items: data,
+    itemsPerPage: 5,
+  })
+
+  const searchData = () => {
+    // if (search !== '') {
+    //   let searchedItem = scoreboard.filter(
+    //     (item) =>
+    //       item.nim.toString().includes(search) ||
+    //       item.name.toLowerCase().includes(search.toLowerCase()) ||
+    //       item.kelompok.toLowerCase().includes(search.toLowerCase())
+    //   );
+    //   setData(searchedItem);
+    // } else {
+    //   setData(defaultData);
+    // }
+    // setPage(1);
+  }
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+
+    // Clicked 'X' on searchbar
+    if (e.nativeEvent.data === undefined) {
+      setData([]);
+      setPage(1);
+    }
+
+    searchData();
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getDataAPI("/customers");
+      setData(data);
+    }
+    fetchData();
+  }, [])
+
   return (
     <PageLayout>
-      <Link to="/dashboard">
-          <Text whiteSpace="pre-line">
-              {`924 Terrell Inlet Apt. 491\nWest Carey, KY 26535`}
-          </Text>
-      </Link>
+      <Container
+        maxW="70ch"
+        mb={8}
+      >
+        <SearchBar />
+      </Container>
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Text
+          fontWeight="bold"
+          mb={5}
+        >
+          Data Customer
+        </Text>
+        {pagination.pageItems.length > 0 ? (
+          pagination.pageItems.map((item, idx) => {
+            return (
+              <DataCard 
+                key={idx}
+                {...item}
+              />
+            )
+        })
+        ) : (
+          <Center>
+            Empty Data
+          </Center>
+        )}
+        <Center>
+          <Pagination
+            {...pagination}
+            onPageChange={(page) => setPage(page)}
+          />
+        </Center>
+      </Flex>
     </PageLayout>
   )
 }
--- src/util/api.js
+++ src/util/api.js
@@ -1,16 +1,62 @@
 import Axios from 'axios';
 
-export async function getDataAPI(path, { authorization }) {
-  if (authorization) {
+export async function newToken() {
+  const { data } = await Axios.post(
+    `${process.env.REACT_APP_API_URL}`+"/auth/login", {
+      email: process.env.REACT_APP_EMAIL,
+      password: process.env.REACT_APP_PASSWORD,
+    }
+  )
+
+  let expire = new Date();
+  expire.setSeconds(expire.getSeconds() + data.expires_in);
+  localStorage.setItem('token', data.access_token);
+  localStorage.setItem('expired', expire.getTime());
+  return data.access_token;
+}
+
+export function getToken() {
+  const token = localStorage.getItem('token');
+  const expired = localStorage.getItem('expired');
+
+  if (token) {
+    let curDate = new Date().getTime();
+    if (curDate <= expired) {
+      return token;
+    }
+  }
+  destroy();
+  return null;
+}
+
+export function destroy() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expired');
+}
+
+export async function getDataAPI(path) {
+  let token = getToken();
+  if (token) {
     const { data } = await Axios.get(
-      `${process.env.REACT_APP_BACKEND_SERVER}${path}`,
+      `${process.env.REACT_APP_API_URL}${path}`,
       {
         headers: {
-          Authorization: `${authorization}`,
+          Authorization: token,
         },
       }
     );
 
     return data;
   }
+
+  token = await newToken();
+  const { data } = await Axios.get(
+    `${process.env.REACT_APP_API_URL}${path}`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  return data
 }
